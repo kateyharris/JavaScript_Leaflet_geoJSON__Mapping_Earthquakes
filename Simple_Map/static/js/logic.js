a7e5be1a3ead0882cb66e5fc8f8d8fefb08365bc
Katey Harris <90797036+kateyharris@users.noreply.github.com>
2021-12-11 12:09:51 -0700

    Update logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -34,7 +34,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://github.com/kateyharris/Mapping_Earthquakes/blob/Mapping_GeoJSON_Points/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/kateyharris/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
