bb4e89d8b24f4dddf86c0c1fc28fffe02a55c33e
Katey Harris <kateyharris9998@gmail.com>
2021-12-11 18:19:57 -0700

    branch

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -22,7 +22,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
+  center: [44.0, -80.0],
   zoom: 2,
   layers: [streets]
 })
@@ -30,12 +30,24 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/kateyharris/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+// Accessing the torontoData GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/kateyharris/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  color: "#ffffa1",
+  weight: 2,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination:" + feature.properties.dst + ",/h3>");
+  }
+}).addTo(map);
 });
