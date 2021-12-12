8ad756bc56dce295cc687e5fbf24b10de2ac0406
Katey Harris <kateyharris9998@gmail.com>
2021-12-11 17:41:26 -0700

    leaflet turtorial

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,22 +2,46 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
-
+var map = L.map('mapid').setView([51.505, -0.09], 13);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11',
+  accessToken: API_KEY,
+  tileSize: 512,
+  zoomOffset: -1
+}).addTo(map);
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map)
+  .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
 
+var circle = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 500
+}).addTo(map).bindPopup('I am a circle.');
+
+var polygon = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(map).bindPopup('I am a polygon.');
+
+
+var popup = L.popup()
+  .setLatLng([51.513, -0.09])
+  .setContent('I am a standalone popup.')
+  .openOn(map);
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent('You clicked the map at ' + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
