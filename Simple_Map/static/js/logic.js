fce2f566a35daebdb4499ac00c86d99a0375439d
Katey Harris <kateyharris9998@gmail.com>
2021-12-14 17:17:03 -0700

    poly complete

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -36,21 +36,17 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/kateyharris/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data, {
-  color: "#ffffa1",
-  weight: 2,
+  fillColor: "yellow",
+  color: "blue",
+  weight: 1,
   onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3><hr><h3> Destination:" + feature.properties.dst + ",/h3>");
+    layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
   }
 }).addTo(map);
 });
